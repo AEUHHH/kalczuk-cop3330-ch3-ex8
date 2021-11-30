9bd0a12613bcc5282885df7f6bdac4bd1e8cc13b
AEUHHH <dr.codycash@gmail.com>
2021-11-29 23:02:44 -0500

    Add project files.

---
+++ kalczuk-cop3330-ch3-ex8/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int number = 0;
+
+    cout << "enter an integer ";
+    if (cin >> number)
+    {
+        if ((number % 2) == 0)
+            cout << "the value " << number << " is even";
+        else
+            cout << "the value " << number << " is odd";
+    }
+    else
+        cout << "the value is invalid";
+
+    return 0;
+}
